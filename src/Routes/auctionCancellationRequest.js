770f6d0d1d41120c791086aaf6a7c189d0599ce3
Thales Burakowski <thalesburakowski@gmail.com>
2019-04-27 22:15:46 -0300

    minor updates

--- src/Routes/auctionCancellationRequest.js
+++ src/Routes/auctionCancellationRequest.js
@@ -1,7 +1,11 @@
 const auctionControllers = require('../Controllers/auctionCancellationRequest')
 
 module.exports = app => {
-	app.route('/leilao-pendente-cancelamento').get(auctionControllers.getAuctionCancellationRequests)
+	app
+		.route('/leilao-pendente-cancelamento')
+		.get(auctionControllers.getAuctionCancellationRequests)
 
-	app.route('/leilao-pendente-cancelamento').post(auctionControllers.approveAuction)
+	app
+		.route('/leilao-pendente-cancelamento')
+		.post(auctionControllers.approveAuction)
 }
