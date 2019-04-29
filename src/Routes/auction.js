6785e60e7e202becc7a8b098c1a2e029f88a5aeb
Thales Burakowski <thalesburakowski@gmail.com>
2019-04-28 22:00:58 -0300

    correcao historico lance

--- src/Routes/auction.js
+++ src/Routes/auction.js
@@ -4,7 +4,7 @@ module.exports = app => {
 	app.route('/leilao').post(auctionControllers.createAuction)
 	app.route('/leilao-aprovado').get(auctionControllers.getApprovedAcutions)
 	app.route('/leilao-lance').post(auctionControllers.bidAuction)
-	app.route('/leilao-historico-lance/:auctionId').post(auctionControllers.getHistoricBids)
+	app.route('/leilao-historico-lance/:auctionId').get(auctionControllers.getHistoricBids)
 	app.route('/leilao-transporte').post(auctionControllers.deliveryAuction)
 
 	app.route('/leilao/:auctionId').get(auctionControllers.getAuction)
