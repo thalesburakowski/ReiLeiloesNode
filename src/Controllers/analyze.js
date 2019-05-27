a8508bd42c11786a53d0164eb6759713ab4ab851
Thales Burakowski <thalesburakowski@gmail.com>
2019-05-27 17:47:11 -0300

    feito auctionsPerCategories

--- src/Controllers/analyze.js
+++ src/Controllers/analyze.js
@@ -1,5 +1,6 @@
 const { prisma } = require('../../generated/prisma-client')
 const { responsePrismaPrismaError } = require('./utils')
+const groupBy = require('group-by')
 
 const auctionsTotal = async (req, res) => {
 	try {
@@ -37,12 +38,95 @@ const auctionsTotal = async (req, res) => {
 
 		const response = {
 			type: 'line',
-			title: 'Quantidade de leilões',
+			title: 'Quantidade de leilões no ultimo mês',
 			labels,
 			datasets: [{ data, label: 'Leilões' }],
 		}
 
 		return res.send(response)
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+/*
+O resultado deve mostrar a quantidade de 
+leilões criado daquela categoria naquele dia
+*/
+const auctionsPerCategories = async (req, res) => {
+	try {
+		const today = new Date()
+		// const actualMonth = new Date().toISOString().split('T')[0]
+		let actualMonth = new Date()
+		actualMonth.setDate(today.getDate() + 1)
+		actualMonth = actualMonth.toISOString().split('T')[0]
+		const lastMonth = new Date(today.setMonth(today.getMonth() - 1))
+			.toISOString()
+			.split('T')[0]
+		const query = `
+    query {
+      auctions(where:{
+        AND: {
+          createdAt_gte: "${lastMonth}"
+          createdAt_lte: "${actualMonth}"
+        }
+      }){
+        categories {
+          name
+        }
+        createdAt
+      }
+    }`
+		const responsePrisma = await prisma.$graphql(query)
+		// console.log(responsePrisma.auctions)
+		const categories = [
+			...new Set(responsePrisma.auctions.map(res => res.categories[0].name)),
+		]
+		const auctions = responsePrisma.auctions.map(res => {
+			return { date: res.createdAt.split('T')[0], categories: res.categories }
+		})
+
+		const auctionsDays = responsePrisma.auctions.map(
+			res => res.createdAt.split('T')[0]
+		)
+
+		const dates = getDates(lastMonth, actualMonth)
+		const labels = dates.map(date => date.split('-')[2])
+		const categoryObj = {}
+
+		categories.map(category => {
+			categoryObj[category] = [...dates]
+		})
+
+		const dataObj = {}
+
+		categories.forEach(category => {
+			dataObj[category] = {}
+			for (let i = 0; i < dates.length; i++) {
+				dataObj[category][dates[i]] = 0
+			}
+		})
+
+		for (let i = 0; i < auctions.length; i++) {
+			auctions[i].categories.forEach(category => {
+				dataObj[category.name][auctions[i].date] =
+					dataObj[category.name][auctions[i].date] + 1
+			})
+		}
+		const response = {
+			type: 'line',
+			title: 'Quantidade de leilões por categoria',
+			labels,
+			datasets: [],
+		}
+		categories.forEach(category => {
+			response.datasets = [
+				...response.datasets,
+				{ label: category, data: [...Object.values(dataObj[category])] },
+			]
+		})
+
+		return res.send(response)	
 	} catch (error) {
 		console.log(error)
 	}
@@ -67,4 +151,5 @@ const getDates = function(startDate, endDate) {
 
 module.exports = {
 	auctionsTotal,
+	auctionsPerCategories,
 }
--- src/Routes/analyze.js
+++ src/Routes/analyze.js
@@ -2,4 +2,5 @@ const analyzeControllers = require('../Controllers/analyze')
 
 module.exports = app => {
   app.route('/analise/leilao-total').get(analyzeControllers.auctionsTotal)
+  app.route('/analise/leilao-por-categoria').get(analyzeControllers.auctionsPerCategories)
 }
